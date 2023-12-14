00ec5abf8af77fa158787cfdd3742beff82b1186
Jakub Petrylak <jakub.petrylak@gmail.com>
2023-12-14 21:53:40 +0100

    Add app constants, https and react utilities and example components.

---
+++ src/constants/app.tsx
@@ -0,0 +1,2 @@
+﻿export const APP_BASE_URL = process.env.APP_BASE_URL;
+export const ESHOP_API_URL = process.env.REACT_APP_ESHOP_API_URL;
---
+++ src/constants/codeStatus.ts
@@ -0,0 +1,4 @@
+﻿export const UNAUTHORIZED = 401;
+export const NOT_FOUND = 404;
+export const TIMEOUT = 408;
+export const OVERRIDE = 409;
